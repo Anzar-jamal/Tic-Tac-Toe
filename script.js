9ab4a5add450affcefc27fe6bb982f247bb58cfa
Anzar Jamal <anzar543210@gmail.com>
2024-10-18 09:27:29 +0530

    init function added

---
+++ script.js
@@ -0,0 +1,35 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelectorAll(".game-info");
+const newGameBtn = document.querySelectorAll(".btn");
+
+
+
+let currentPlayer;
+let gameGrid;
+
+
+const winningPositions = [
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+
+];
+
+
+function initGame () {
+
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `current Player - ${currentPlayer}`;
+
+
+}
+
+initGame();
